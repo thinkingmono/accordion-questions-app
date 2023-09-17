87e20af3e77a274e9045d499f676e807ab6173af
Alejandro Garcia <thinkingmono92@gmail.com>
2023-09-16 19:00:50 -0500

    Instructor better approach to leave one question active. App.js | Adding activeId state variable to track active question id. Adding toggleQuestion function to control if question shows or not their info. Pass activeId and toggleQuestion to singleQuestion component. SingleQuestion.jsx | isActive const variable addition to track locally if question is active or not. Info conditional render now based on isActive variable

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,25 @@ import SingleQuestion from "./SingleQuestion";
 
 const App = () => {
   const [qstn, setQstn] = useState(questions);
-  const isActive = (id) => {
-    let state =  false;
-    const newArray = [...qstn];
-    newArray.map((question) => {
-      if (id === question.id) {
-        question.active = true;
-        state = question.active;
-      } else {
-        question.active = false;
-      }
-    })
-    // console.log(newArray);
-    setQstn(newArray);
-    return state;
-  }
+  const [activeId, setActiveId] = useState(null);
 
-  useEffect(() => {
-    const addIsActive = () => {
-      qstn.map((question) => {
-        question.active = false;
-      })
-    }
-    addIsActive();
-  }, []);
+  const toggleQuestion = (id) => {
+    const newActiveId = id === activeId ? null : id;
+    setActiveId(newActiveId);
+  }
 
   return (
     <>
       <main>
-        <div className="container">
+        <section className="container">
           <h1>Questions</h1>
           {qstn ?
             qstn.map((question) => {
               const { id } = question;
-              return <SingleQuestion {...question} key={id} isActive={isActive} />
+              return <SingleQuestion {...question} key={id} activeId={activeId} toggleQuestion={toggleQuestion} />
             })
             : <h3>There are no questions</h3>}
-        </div>
+        </section>
       </main>
     </>
   );
--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,24 +1,16 @@
-import { useState } from 'react';
 import { FaPlus } from '../node_modules/react-icons/fa';
 import { FaMinus } from '../node_modules/react-icons/fa';
 
-const SingleQuestion = ({ id, title, info, active, isActive }) => {
-    const [showInfo, setShowInfo] = useState(false);
+const SingleQuestion = ({ id, title, info, activeId, toggleQuestion }) => {
+    const isActive = id === activeId;
 
     return (
         <article className="question">
             <header>
                 <h5>{title}</h5>
-                <button type="button" className="question-btn" onClick={() => {
-                    if (active && showInfo) {
-                        active = false;
-                        setShowInfo(false);
-                    } else {
-                        setShowInfo(isActive(id));
-                    }
-                }}>{showInfo && active ? <FaMinus /> : <FaPlus />}</button>
+                <button type="button" className="question-btn" onClick={() => toggleQuestion(id)}>{isActive ? <FaMinus /> : <FaPlus />}</button>
             </header>
-            {showInfo && active ? <p>{info}</p> : null}
+            {isActive && <p>{info}</p>}
         </article>
     )
 }
